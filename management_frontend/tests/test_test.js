c6c96543d70b9274aee9ae6e837ba82da79ff49c
paulsnar <paulsnar@gmail.com>
2015-12-28 01:16:03 +0200

    add dummy suite, jshint ignore
    beginning to get prepared for testing!

---
+++ management_frontend/tests/test_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('First suite', function() {
+    it('should not fail ever', function() {
+        expect(true).toBe(true);
+    });
+    // it('should fail always', function() {
+    //     expect(1).toBe(5);
+    // });
+});
