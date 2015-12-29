e777dfd51b27dbdbb2d98655fd306e37f4db190d
paulsnar <paulsnar@gmail.com>
2015-12-29 16:08:02 +0200

    revamp tests for DefaultChange
    inspired by the new DefaultTimetable tests, I think these might
    be one of the oldest ones I have.
    
    Anyway, renderIntoDocument is awesome <3

--- management_frontend/src/components/__tests__/DefaultChange.test.js
+++ management_frontend/src/components/__tests__/DefaultChange.test.js
@@ -7,44 +7,68 @@ describe('DefaultChange', function() {
     var ReactTestUtils = require('react-addons-test-utils');
     var rewire = require('rewire');
 
-    var DefaultChange;
     var change = {
         id: '12345678901234567890',
         for_class: 'dummy class',
         for_date: '2015-12-01'
     };
 
+    var DefaultChange;
     beforeEach(function() {
         DefaultChange = rewire('../DefaultChange');
     });
 
     it('should render when passed a Change object', function() {
-        var Renderer = ReactTestUtils.createRenderer();
-        Renderer.render(<DefaultChange change={change} />);
-        var rendered = Renderer.getRenderOutput();
-
-        expect(rendered).toBeDefined();
-        expect(rendered.type).toEqual('span');
+        expect(() => ReactTestUtils.renderIntoDocument(<DefaultChange
+            change={change} />)).not.toThrow();
     });
 
-    it('should call removeCallback once the ConfirmActionButton is clicked twice', function(done) {
-        DefaultChange.__set__('Data', {
+    it('should call Data.changes.delete when ConfirmActionButton is clicked twice', function() {
+        var Data = {
             changes: {
-                delete: function(id) {
-                    return new Promise((res, rej) => {
-                        res({ success: true });
-                    });
+                delete: function() {
+                    return new Promise(res => res({ success: true }));
                 }
             }
-        });
-        var component = ReactTestUtils.renderIntoDocument(
-            <DefaultChange change={change} removeCallback={done} />);
-        // var rendered = ReactDOM.findDOMNode(component);
-        // var confirmActionButton = rendered.children[0];
-        var confirmActionButton = ReactTestUtils.findRenderedDOMComponentWithTag(
-            component, 'button');
+        };
+        var spy = sinon.spy(Data.changes, 'delete');
+        DefaultChange.__set__('Data', Data);
 
-        ReactTestUtils.Simulate.click(confirmActionButton);
-        ReactTestUtils.Simulate.click(confirmActionButton);
+        var component = ReactTestUtils.renderIntoDocument(<DefaultChange
+            change={change} />);
+        var btn = ReactTestUtils.findRenderedComponentWithType(component,
+            DefaultChange.__get__('ConfirmActionButton'));
+        var btnNode = ReactDOM.findDOMNode(btn);
+        ReactTestUtils.Simulate.click(btnNode);
+        ReactTestUtils.Simulate.click(btnNode);
+
+        expect(spy.called).toEqual(true);
+    });
+
+    it('should call self-removal callback once the delete operation succeeds', function(done) {
+        var Data = {
+            changes: {
+                delete: function() {
+                    return new Promise(res => res({ success: true }));
+                }
+            }
+        };
+        DefaultChange.__set__('Data', Data);
+
+        var spy = sinon.spy();
+        var component = ReactTestUtils.renderIntoDocument(<DefaultChange
+            change={change} removeCallback={spy} />);
+
+        var btn = ReactTestUtils.findRenderedComponentWithType(component,
+            DefaultChange.__get__('ConfirmActionButton'));
+        var btnNode = ReactDOM.findDOMNode(btn);
+        ReactTestUtils.Simulate.click(btnNode);
+        ReactTestUtils.Simulate.click(btnNode);
+
+        setTimeout(function() {
+            expect(spy.called).toEqual(true);
+
+            done();
+        }, 50);
     });
 });
