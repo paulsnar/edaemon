ede99e1d44ed8f239c50c2e37243dda429b4cf7d
paulsnar <paulsnar@gmail.com>
2016-01-01 17:56:20 +0200

    add notice on update availability

--- management_frontend/src/routes/index/index.js
+++ management_frontend/src/routes/index/index.js
@@ -4,14 +4,45 @@
 
 var React = require('react');
 
+var _ = require('lodash');
+var Data = require('../../data');
+
 var ChangesColumn = require('./ChangesColumn');
 var TimetablesColumn = require('./TimetablesColumn');
 
 var IndexHandler = React.createClass({
+    getInitialState: function() {
+        return { update: false };
+    },
+    componentDidMount: function() {
+        this.data = { version: { } };
+        Data.meta.checkUpdates()
+        .then(resp => {
+            if (!_.isEqual(resp.current_version, resp.latest_version)) {
+                this.data.version = {
+                    current: resp.current_version.join('.'),
+                    latest: resp.latest_version.join('.')
+                };
+                this.setState({ update: true });
+            }
+        })
+        .catch(err => { /* just do nothing… */ });
+    },
     render: function() {
+        var _update;
+        if (this.state.update) {
+            _update = <div className="alert alert-info">
+                Jūsu Edaemon instancei ir pieejams atjauninājums.
+                Jūs pašlaik izmantojat versiju {this.data.version.current},
+                bet ir pieejama versija {this.data.version.latest}.
+                <a href="https://github.com/paulsnar/edaemon">Spiediet šeit</a>,
+                lai dotos uz Edaemon Github repozitoriju.
+            </div>;
+        }
         /*jshint ignore:start */
         return <div>
             <h1>Pārvaldīšana</h1>
+            {_update}
             <div className="row">
                 <ChangesColumn className="col-md-6" />
                 <TimetablesColumn className="col-md-6" />
