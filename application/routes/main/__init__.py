7b6b2779609a7c8475af19b4507b2a9859da8d27
paulsnar <paulsnar@paulsnar.lv>
2016-02-12 17:23:50 +0200

    use proper ISO8601 validation in route

--- application/routes/main/__init__.py
+++ application/routes/main/__init__.py
@@ -8,6 +8,6 @@ from .changes_for_class import ChangesForClass
 all_routes = [
     ('/', AllChanges),
     ('/changes/show/([0-9A-Za-z\-_]+)', ShowChange),
-    ('/changes/by_date/([0-9-]{10})', ChangesForDate),
+    ('/changes/by_date/([0-9]{4}-[0-9]{2}-[0-9]{2})', ChangesForDate),
     ('/changes/for_class/([0-9A-Za-z\.]+)', ChangesForClass),
 ]
