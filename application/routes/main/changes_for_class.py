b7c05ca4883e83dbc9ac75b0679d3c226ab50b35
paulsnar <paulsnar@paulsnar.lv>
2016-02-12 17:20:15 +0200

    get only a week in /changes/for_class/
    Fixes #28

--- application/routes/main/changes_for_class.py
+++ application/routes/main/changes_for_class.py
@@ -9,6 +9,6 @@ from ...models import Change
 class ChangesForClass(Handler):
     def get(self, for_class):
         template = env.get_template('main/changes_for_class.htm')
-        changes = Change.get_for_class(for_class)
+        changes = Change.get_week_for_class(for_class)
         self.response.write(template.render(
             changes=changes, for_class=for_class))
