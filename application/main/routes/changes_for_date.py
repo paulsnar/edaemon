c75f6c1eb55a62dfcea21dce79a34ca83fa0a7d8
paulsnar <paulsnar@paulsnar.lv>
2016-05-12 20:25:53 +0300

    Add missing env import

--- application/main/routes/changes_for_date.py
+++ application/main/routes/changes_for_date.py
@@ -5,6 +5,8 @@ import webapp2
 from application.common import BaseHandler
 from application.common.models import Change
 
+from ..templates import environment
+
 class ChangesForDate(BaseHandler):
     def get(self, date):
         template = environment.get_template('changes_for_date.htm')
