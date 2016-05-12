7a0bb10e449f3fd78a4f9fb24bd7e7ad1c82e976
paulsnar <paulsnar@paulsnar.lv>
2016-05-12 21:13:27 +0300

    Add v1 specific welcome
    This has a method map, which will be the recommended way to
    interact with the API.

--- application/api/routes/v1/__init__.py
+++ application/api/routes/v1/__init__.py
@@ -2,7 +2,23 @@
 
 from .changes import AllChanges, SpecificChange, ChangesForClass, ChangesForDate
 
+from ..handler import BaseHandler
+class ApiVersionWelcome(BaseHandler):
+    def get(self):
+        self.jsonify(
+            success=True,
+            message='Edaemon v1 API welcomes you',
+            version=1,
+            method_map={
+                'changes.all.get': '/api/v1/changes',
+                'changes.one.get': '/api/v1/changes/<id>',
+                'changes.for_date.get': '/api/v1/changes/for_date/<date>',
+                'changes.for_class.get': '/api/v1/changes/for_class/<class>'
+            }
+        )
+
 v1_routes = [
+    (r'/api/v1/', ApiVersionWelcome),
     (r'/api/v1/changes', AllChanges),
     (r'/api/v1/changes/([0-9A-Za-z\-_]+)', SpecificChange),
     (r'/api/v1/changes/for_date/([0-9]{4}-[0-9]{2}-[0-9]{2})', ChangesForDate),
