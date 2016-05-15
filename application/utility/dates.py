bac0ea67eefe68b3627285adea9a4cf0ef7dd7c1
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 21:49:44 +0300

    Rename ISO8601 methods to an object
    I consider this cleaner

--- application/utility/dates.py
+++ application/utility/dates.py
@@ -3,11 +3,14 @@
 import re
 from datetime import date, timedelta
 
-def ISO8601_parse(_date):
-    return date(int(_date[0:4]), int(_date[5:7]), int(_date[8:10]))
+class ISO8601(object):
+    @staticmethod
+    def parse(_date):
+        return date(int(_date[0:4]), int(_date[5:7]), int(_date[8:10]))
 
-def check_ISO8601_compliance(_date):
-    if re.search(r'\d{4}-\d{2}-\d{2}', _date):
-        return True
-    else:
-        return False
+    @staticmethod
+    def is_valid(_date):
+        if re.search(r'\d{4}-\d{2}-\d{2}', _date):
+            return True
+        else:
+            return False
