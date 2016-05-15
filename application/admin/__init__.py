86bcc74f5fe2c5f6f0de6f2de8765d2cb16f79a7
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 13:53:25 +0300

    Remove admin altogether
    Let's make a separate frontend which doesn't require deviation from
    built-in APIs!

--- application/admin/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from application.common import config as common_config, default_404_handler
-import webapp2
-
-wsgi_app = webapp2.WSGIApplication(routes=[], config=common_config)
-wsgi_app.error_handlers[404] = default_404_handler
