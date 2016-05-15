40328b7d1fe837aa595757cf7efd7894792fb352
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 11:31:10 +0300

    Remove a lot of boilerplate from API handlers

--- application/api/routes/handler.py
+++ application/api/routes/handler.py
@@ -3,6 +3,7 @@
 import logging
 import webapp2
 import json
+from google.appengine.datastore.datastore_query import Cursor
 
 class BaseHandler(webapp2.RequestHandler):
     def jsonify(self, **kwargs):
@@ -12,4 +13,61 @@ class BaseHandler(webapp2.RequestHandler):
 
     def handle_exception(self, exception, debug):
         logging.exception(exception)
-        
+
+    def collection_method(self, collection, kind, projection=None):
+        try:
+            if hasattr(self, 'default_page_size'):
+                default_page_size = self.default_page_size
+            else:
+                default_page_size = 15
+            if hasattr(self, 'max_page_size'):
+                max_page_size = self.max_page_size
+            else:
+                max_page_size = 20
+
+            if self.request.get('page_size'):
+                try:
+                    page_size = int(self.request.get('page_size'))
+                    if page_size < 0:
+                        page_size = default_page_size
+                    elif page_size > max_page_size:
+                        page_size = max_page_size
+                except ValueError:
+                    page_size = default_page_size
+            else:
+                page_size = default_page_size
+
+            if self.request.get('cursor'):
+                c = Cursor(urlsafe=self.request.get('cursor'))
+                results, next_c, more = collection.fetch_page(page_size,
+                    start_cursor=c,
+                    projection=projection)
+            else:
+                results, next_c, more = collection.fetch_page(page_size,
+                    projection=projection)
+
+            request_meta = dict(
+                page_size=page_size,
+                item_count=len(results)
+            )
+            if more and next_c:
+                request_meta['has_more'] = True
+                request_meta['next_cursor'] = next_c.urlsafe()
+            else:
+                request_meta['has_more'] = False
+
+            self.jsonify(
+                success=True,
+                kind=kind,
+                items=[result.to_dict() for result in results],
+                request_meta=request_meta
+            )
+        except Exception:
+            self.response.set_status(500),
+            self.jsonify(
+                success=False,
+                error=True,
+                code=500,
+                message='Server-side error'
+            )
+            raise
--- application/api/routes/v1/changes.py
+++ application/api/routes/v1/changes.py
@@ -9,50 +9,11 @@ from application.common.models import Change
 
 class AllChanges(BaseHandler):
     def get(self):
-        try:
-            page_size = 15
-            if self.request.get('page_size'):
-                try:
-                    page_size = int(self.request.get('page_size'))
-                    if page_size < 0:
-                        page_size = 15
-                    elif page_size > 20:
-                        page_size = 20
-                except ValueError:
-                    pass
-            if self.request.get('cursor'):
-                c = Cursor(urlsafe=self.request.get('cursor'))
-                changes, next_c, more = Change.get_all().fetch_page(page_size,
-                    start_cursor=c,
-                    projection=[Change.for_class, Change.for_date])
-            else:
-                changes, next_c, more = Change.get_all().fetch_page(page_size,
-                    projection=[Change.for_class, Change.for_date])
-            
-            request_meta = dict(
-                page_size=page_size,
-                item_count=len(changes)
-            )
-            if more and next_c:
-                request_meta['has_more'] = True
-                request_meta['next_cursor'] = next_c.urlsafe()
-            else:
-                request_meta['has_more'] = False
-            
-            self.jsonify(
-                success=True,
-                kind='Change',
-                items=[change.to_dict() for change in changes],
-                request_meta=request_meta
-            )
-        except Exception:
-            self.response.set_status(500)
-            self.jsonify(
-                success=False,
-                error=True,
-                code=500,
-                message='Server-side error')
-            raise
+        return self.collection_method(
+            collection=Change.get_all(), 
+            kind='Change',
+            projection=[Change.for_class, Change.for_date]
+        )
 
 class SpecificChange(BaseHandler):
     def get(self, change_id):
@@ -86,98 +47,16 @@ class SpecificChange(BaseHandler):
 
 class ChangesForClass(BaseHandler):
     def get(self, class_name):
-        try:
-            page_size = 15
-            if self.request.get('page_size'):
-                try:
-                    page_size = int(self.request.get('page_size'))
-                    if page_size < 0:
-                        page_size = 15
-                    elif page_size > 20:
-                        page_size = 20
-                except ValueError:
-                    pass
-            if self.request.get('cursor'):
-                c = Cursor(urlsafe=self.request.get('cursor'))
-                changes, next_c, more = Change.get_for_class(
-                    class_name).fetch_page(page_size,
-                    projection=[Change.for_date])
-            else:
-                changes, next_c, more = Change.get_for_class(
-                    class_name).fetch_page(page_size,
-                    projection=[Change.for_date])
-
-            request_meta = dict(
-                page_size=page_size,
-                item_count=len(changes)
-            )
-            if more and next_c:
-                request_meta['has_more'] = True
-                request_meta['next_cursor'] = next_c.urlsafe()
-            else:
-                request_meta['has_more'] = False
-
-            self.jsonify(
-                success=True,
-                kind='Change',
-                items=[change.to_dict() for change in changes],
-                request_meta=request_meta
-            )
-        except Exception:
-            self.response.set_status(500)
-            self.jsonify(
-                success=False,
-                error=True,
-                code=500,
-                message='Server-side error'
-            )
-            raise
+        return self.collection_method(
+            collection=Change.get_for_class(class_name),
+            kind='Change',
+            projection=[Change.for_date]
+        )
 
 class ChangesForDate(BaseHandler):
     def get(self, date):
-        try:
-            page_size = 15
-            if self.request.get('page_size'):
-                try:
-                    page_size = int(self.request.get('page_size'))
-                    if page_size < 0:
-                        page_size = 15
-                    elif page_size > 20:
-                        page_size = 20
-                except ValueError:
-                    pass
-            if self.request.get('cursor'):
-                c = Cursor(urlsafe=self.request.get('cursor'))
-                changes, next_c, more = Change.get_for_date(
-                    date).fetch_page(page_size,
-                    projection=[Change.for_class])
-            else:
-                changes, next_c, more = Change.get_for_date(
-                    date).fetch_page(page_size,
-                    projection=[Change.for_class])
-
-            request_meta = dict(
-                page_size=page_size,
-                item_count=len(changes)
-            )
-            if more and next_c:
-                request_meta['has_more'] = True
-                request_meta['next_cursor'] = next_c.urlsafe()
-            else:
-                request_meta['has_more'] = False
-
-            self.jsonify(
-                success=True,
-                kind='Change',
-                items=[change.to_dict() for change in changes],
-                request_meta=request_meta
-            )
-        except Exception:
-            self.response.set_status(500)
-            self.jsonify(
-                success=False,
-                error=True,
-                code=500,
-                message='Server-side error'
-            )
-            raise
+        return self.collection_method(
+            collection=Change.get_for_date(date),
+            kind='Change',
+            projection=[Change.for_class]
+        )
