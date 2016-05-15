1f90ac73f6fc9a6be929927614c59d1e4bed7263
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 22:17:26 +0300

    Remove some long overdue code

--- application/utility/lesson.py
+++ application/utility/lesson.py
@@ -1,20 +1,5 @@
 # coding: utf-8
 
-class lesson_utils(object):
-    @staticmethod
-    def process_form(form):
-        # for the static thing
-        subjects = [ ]
-        for i in xrange(0, 9):
-            subject = form.get('lesson-{0}'.format(i), None)
-            if type(subject == unicode) and \
-            subject == u'-' or subject == u'':
-                subjects.append(None)
-            else:
-                subjects.append(subject)
-        return subjects
-
-
 def trim_trailing_nulls(lesson_list):
     new_list = list(lesson_list)
     for i in reversed(lesson_list):
