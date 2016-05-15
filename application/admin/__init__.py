a0cf1770f79ff8ea236b5232275b35bf9b7ee9e7
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 12:22:11 +0300

    Replace admin with a whiteroom implementation

--- application/admin/__init__.py
+++ application/admin/__init__.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 
 from application.common import config as common_config, default_404_handler
-from .routes import all_routes as admin_routes
 import webapp2
 
-wsgi_app = webapp2.WSGIApplication(routes=admin_routes, config=common_config)
+wsgi_app = webapp2.WSGIApplication(routes=[], config=common_config)
 wsgi_app.error_handlers[404] = default_404_handler
--- application/admin/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from jinja2 import Environment, PackageLoader
-from application.common import common_globals
-
-environment = Environment(loader=PackageLoader(__name__, '.'))
-environment.globals = common_globals
