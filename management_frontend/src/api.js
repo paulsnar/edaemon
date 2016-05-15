b86239a0af543ec78b62a1f19667facd3b162e0b
paulsnar <paulsnar@paulsnar.lv>
2016-05-15 18:07:24 +0300

    Fix api route being wrong

--- management_frontend/src/api.js
+++ management_frontend/src/api.js
@@ -5,20 +5,20 @@ import 'whatwg-fetch';
 const API = {
     Change: {
         delete(id) {
-            let ret = fetch(`/api/changes/${id}`, {
+            let ret = fetch(`/api/v1/changes/${id}`, {
                 credentials: 'same-origin',
                 method: 'DELETE'
             })
             .then(res => res.json())
             .catch(err => {
                 console.error('[api] error', err,
-                    `on request: DELETE /api/changes/${id}`);
+                    `on request: DELETE /api/v1/changes/${id}`);
             });
 
             ret.then(body => {
                 if (!body.success) {
                     console.warn('[api] unsuccessful operation',
-                        `on request: DELETE /api/changes/${id}`);
+                        `on request: DELETE /api/v1/changes/${id}`);
                     console.debug('[api] included body:', body);
                 }
             });
@@ -27,3 +27,5 @@ const API = {
         }
     }
 }
+
+module.exports = API;
