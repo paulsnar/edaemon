465cd78866c79f90443cbf79445cf07aebc97f57
paulsnar <paulsnar@paulsnar.lv>
2016-05-16 15:57:47 +0300

    Add explainer to trashlist

--- management_frontend/src/DeletedChangeList.js
+++ management_frontend/src/DeletedChangeList.js
@@ -19,18 +19,26 @@ class DeletedChangeListComponent extends React.Component {
         this.setState({ items });
     }
     render() {
-        return <ul>
-            {this.state.items.map((item, i) =>
-            <li key={item.id}>
-                <button className="btn btn-default btn-xs"
-                    onClick={this._handleChildRemoving.bind(this, i)}>
-                    <span className="glyphicon glyphicon-arrow-left" />
-                </button>
+        if (this.state.items.length === 0) {
+            return <p className="text-center text-muted">
+                Te parādās dzēstās izmaiņas, un no šejienes tās var atjaunot atpakaļ.
                 {' '}
-                Datums: {item.for_date}, klase: {item.for_class}
-            </li>
-            )}
-        </ul>;
+                Ja tās netiek atgrieztas 3 dienu laikā, tās tiek izdzēstas neatgriezeniski.
+            </p>;
+        } else {
+            return <ul>
+                {this.state.items.map((item, i) =>
+                <li key={item.id}>
+                    <button className="btn btn-default btn-xs"
+                        onClick={this._handleChildRemoving.bind(this, i)}>
+                        <span className="glyphicon glyphicon-arrow-left" />
+                    </button>
+                    {' '}
+                    Datums: {item.for_date}, klase: {item.for_class}
+                </li>
+                )}
+            </ul>;
+        }
     }
 }
 
