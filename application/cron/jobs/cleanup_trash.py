abee9a27d422c5564bf03eba652b92804aa7a9b7
paulsnar <paulsnar@paulsnar.lv>
2016-05-16 11:06:30 +0300

    Map to a lambda instead

--- application/cron/jobs/cleanup_trash.py
+++ application/cron/jobs/cleanup_trash.py
@@ -8,6 +8,4 @@ def run():
     earliest = date.today() - timedelta(days=3)
     purgable = Change.query(Change.purgeable_since != None,
         Change.purgeable_since < earliest)
-    def drop_all(msg):
-        msg.key.delete()
-    purgable.map(drop_all)
+    purgable.map(lambda i: i.key.delete())
