35a84b9032e9127191a521b2eb8a53c28aa13607
paulsnar <paulsnar@paulsnar.lv>
2016-05-16 16:03:32 +0300

    Move everything out of application/__init__.py
    There's no need for such duplication, I'd rather let the entry
    points be looked up by app.yaml on demand

--- application/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-from .admin import wsgi_app as admin_wsgi_endpoint
-from .api import wsgi_app as api_wsgi_endpoint
-from .main import wsgi_app as main_wsgi_endpoint
-from .cron import wsgi_app as cron_wsgi_endpoint
