7e9b6fd73c0777f52751a543a7a1a4a737c25ad8
paulsnar <paulsnar@paulsnar.lv>
2016-05-17 14:29:56 +0300

    Groundwork for notifications

--- application/api/routes/v1/__init__.py
+++ application/api/routes/v1/__init__.py
@@ -2,6 +2,7 @@
 
 from .changes import AllChanges, SpecificChange, ChangesForClass, ChangesForDate
 from .trash import TrashHandler
+from .notify import NotifyHandler
 
 from ..handler import BaseHandler
 class ApiVersionWelcome(BaseHandler):
@@ -27,5 +28,7 @@ v1_routes = [
     (r'/api/v1/changes/for_date/([0-9]{4}-[0-9]{2}-[0-9]{2})', ChangesForDate),
     (r'/api/v1/changes/for_class/([0-9A-Za-z\.]+)', ChangesForClass),
 
-    (r'/api/v1/trash', TrashHandler)
+    (r'/api/v1/trash', TrashHandler),
+
+    (r'/api/v1/notify', NotifyHandler)
 ]
